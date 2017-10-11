5df10f55a01de0f585b11f5bc7eab739cb217451
git@oschina <shungdawei@foxmail.com>
2017-10-11 19:45:15 +0800

    旧项目同步

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/index.css" />
+    <title>reitoo studio</title>
+</head>
+  <body id="w-body-aaaa" data-type="body" class="n-widget w-body t-body-default-aaab">
+    <div id="w-area-aaac" data-type="area" class="n-widget w-area">
+      <div id="w-text-aaad" data-type="text" class="n-widget w-text">
+      <div style="text-align: center;"><font color="#ffffff">首页 &nbsp; | &nbsp; 科技 &nbsp; | &nbsp; 手机 &nbsp; | &nbsp; 数码 &nbsp; | &nbsp; 论坛 &nbsp; </font></div>
+      </div>
+    </div>
+    <div id="w-columnbox-aaae" data-type="columnbox" class="n-widget w-columnbox">
+        <div class="w-col" style="width:50%;">
+        <div id="w-area-aaaf" data-type="area" class="n-widget w-area">
+        </div>
+        </div>
+        <div class="w-col" style="width:50%;">
+        <div id="w-area-aaag" data-type="area" class="n-widget w-area">
+          <div id="w-area-aaai" data-type="area" class="n-widget w-area">
+            <div id="w-text-aaaj" data-type="text" class="n-widget w-text">
+            <h1 class="inlinediting"><font color="#ffffff" size="7">移动客户端</font></h1>
+            </div>
+          </div>
+          <div id="w-text-aaak" data-type="text" class="n-widget w-text">
+          <font color="#ffffff" size="3">CmsTop在新媒体行业推出的移动互联网产品，提供了包含新闻、组图、视频、专题等多种内容展现阅读方式，并支持基于web的应用广场扩展，能够帮助新媒体站点快速建立功能多样的移动阅读产品。</font>
+          </div>
+          <div id="w-image-aaal" data-type="image" class="n-widget w-image">
+            <a href="http://m.cmstop.com/" target="_blank"><img src="assets/19daa85a12c9b6f9f759e38889fb6c12.png" /></a>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div id="w-area-aaam" data-type="area" class="n-widget w-area">
+      <div id="w-columnbox-aaan" data-type="columnbox" class="n-widget w-columnbox">
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aaao" data-type="area" class="n-widget w-area">
+            <div id="w-text-aaap" data-type="text" class="n-widget w-text">
+            <font size="6">愉悦的阅读体验</font>
+            </div>
+            <div id="w-area-aaaq" data-type="area" class="n-widget w-area">
+              <div id="w-text-aaar" data-type="text" class="n-widget w-text">
+              界面极简，完美地支持手势操作，频道导航采用抽屉式设计，使用户更加专注于内容阅读。
+              </div>
+            </div>
+          </div>
+          </div>
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aaas" data-type="area" class="n-widget w-area">
+            <div id="w-image-aaat" data-type="image" class="n-widget w-image">
+              <img src="assets/c30254061d67b5c0f0db1da83f4ac47b.png" />
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    <div id="w-area-aaau" data-type="area" class="n-widget w-area">
+      <div id="w-columnbox-aaav" data-type="columnbox" class="n-widget w-columnbox">
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aaaw" data-type="area" class="n-widget w-area">
+            <div id="w-image-aaax" data-type="image" class="n-widget w-image">
+              <img src="assets/5d07ed705b14f2dbfce1f97ff050975a.png" />
+            </div>
+          </div>
+          </div>
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aaay" data-type="area" class="n-widget w-area">
+            <div id="w-text-aaaz" data-type="text" class="n-widget w-text">
+            <font size="6">丰富的内容展现形式</font><br>
+            </div>
+            <div id="w-area-aaba" data-type="area" class="n-widget w-area">
+              <div id="w-text-aabb" data-type="text" class="n-widget w-text">
+              支持新闻、组图、视频、专题等多种内容类型，内容中还可以通过挂件功能嵌入其他类型的信息，为编辑提供更加灵活的内容加工呈现手段，为读者提供了更高质量的内容。
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    <div id="w-area-aabc" data-type="area" class="n-widget w-area">
+      <div id="w-columnbox-aabd" data-type="columnbox" class="n-widget w-columnbox">
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabe" data-type="area" class="n-widget w-area">
+            <div id="w-text-aabf" data-type="text" class="n-widget w-text">
+            <font size="6">消息实时推送</font><br>
+            </div>
+            <div id="w-area-aabg" data-type="area" class="n-widget w-area">
+              <div id="w-text-aabh" data-type="text" class="n-widget w-text">
+              支持内容、公告、广告等各类信息的即时与定时推送，最新消息移动端第一时间即可收到。
+              </div>
+            </div>
+          </div>
+          </div>
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabi" data-type="area" class="n-widget w-area">
+            <div id="w-image-aabj" data-type="image" class="n-widget w-image">
+              <img src="assets/76e842cf002663ef1e55d371115a185f.png" />
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    <div id="w-area-aabk" data-type="area" class="n-widget w-area">
+      <div id="w-columnbox-aabl" data-type="columnbox" class="n-widget w-columnbox">
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabm" data-type="area" class="n-widget w-area">
+            <div id="w-image-aabn" data-type="image" class="n-widget w-image">
+              <img src="assets/65482ffcb4a8046e036a0afd808e92b9.jpg" />
+            </div>
+          </div>
+          </div>
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabo" data-type="area" class="n-widget w-area">
+            <div id="w-text-aabp" data-type="text" class="n-widget w-text">
+            <font size="6">移动广告盈利机制</font><br>
+            </div>
+            <div id="w-area-aabq" data-type="area" class="n-widget w-area">
+              <div id="w-text-aabr" data-type="text" class="n-widget w-text">
+              支持启动画面、列表页幻灯片、文章内容页广告投放与管理，助力站点快速实现盈利。
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    <div id="w-area-aabs" data-type="area" class="n-widget w-area">
+      <div id="w-columnbox-aabt" data-type="columnbox" class="n-widget w-columnbox">
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabu" data-type="area" class="n-widget w-area">
+            <div id="w-text-aabv" data-type="text" class="n-widget w-text">
+            <font color="#ffffff">首页 &nbsp; | &nbsp; 关于我们 &nbsp;</font>
+            </div>
+          </div>
+          </div>
+          <div class="w-col" style="width:50%;">
+          <div id="w-area-aabw" data-type="area" class="n-widget w-area">
+            <div id="w-text-aabx" data-type="text" class="n-widget w-text">
+            <div style="text-align: right;"><font color="#ffffff">REITOO STUDIO</font><br></div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
